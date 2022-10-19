02f40fdaa1723ff974d7d719e51dbb1eeacf8e23
Andrei7778 <sabauandreu@gmail.com>
2022-10-19 17:22:36 +0300

    SLOW(Quick) fox QUICK dog

--- src/ro/uvt/p3/io/InputDevice.java
+++ src/ro/uvt/p3/io/InputDevice.java
@@ -3,6 +3,6 @@ package ro.uvt.p3.io;
 public class InputDevice {
 
     public String nextLine(){
-        return "The SLOW brown fox jumps over the QUCK dog.";
+        return "The SLOW brown fox jumps over the QUICK dog.";
     }
 }
